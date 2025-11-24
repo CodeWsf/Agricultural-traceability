a6ac611cc8f8d5f4fb765854d564e2e99ea0c760
mygit <1273407559@qq.com>
2025-11-24 14:39:13 +0800

    第二次提交

--- src/main/java/com/briup/product_source/dao/IssueRecordMapper.java
+++ src/main/java/com/briup/product_source/dao/IssueRecordMapper.java
@@ -1,5 +1,6 @@
 package com.briup.product_source.dao;
 
+import org.apache.ibatis.annotations.MapKey;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -7,6 +8,6 @@ import java.util.Map;
 
 @Repository
 public interface IssueRecordMapper {
-
+    @MapKey("月份")
     List<Map<String, Integer>> countSales();
 }
